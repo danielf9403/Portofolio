d476fad8a53046047934169633b55f0c7220201b
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-04-12 14:12:32 -0400

    3D-carousel auto-spin complete

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -6,11 +6,17 @@ function projects() {
   return (
        <div className="p-container">
         <div className="p-carousel">
-            <div className="p-box"><span>Project1</span></div>
+            <div className="p-box">
+                <span>Pac-Man</span>
+                <a href=""></a>
+                <img src="" alt="pacman" />
+            </div>
             <div className="p-box"><span>Project2</span></div>
             <div className="p-box"><span>Project3</span></div>
             <div className="p-box"><span>Project4</span></div>
             <div className="p-box"><span>Project5</span></div>
+            <div className="p-box"><span>Project6</span></div>
+
         </div>
        </div>
   );
