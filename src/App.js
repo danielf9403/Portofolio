2a4ee4d5744943e7b7d752409931c64ba7485539
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-04-12 18:47:48 -0400

    css

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ import './style.css';
 function App() {
   return(
     
-      <div className="c-body">
+      <div className="app-body">
         <Router>
-          <div className="c-nav">
+          <div className="app-nav">
             <NavBar />
           </div>
-          <div className="c-wrap">
-            <div className="c-side">
+          <div className="app-wrap">
+            <div className="app-side">
               <Sidebar/>
             </div>
-            <div className="c-main">
+            <div className="app-main">
                 <Routes>
                   <Route exact path="/" element={<Home/>} />
                   <Route path="/contact" element={<Contact/>} />
