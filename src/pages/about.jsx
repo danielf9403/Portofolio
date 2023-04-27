a6b62a01bcabfa572e21dcc129414b7ab0c87cda
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-04-26 21:54:29 -0400

    need pics and text about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import AboutCarousel from "../components/aboutCarousel";
 const About = () => {
     return(
-    <div className="about">
-        <h1>about me </h1>
-    </div>
+        <div className="about">
+            <div className="about-pic">
+                <AboutCarousel/>
+            </div>
+            <div className="about-name-paragraf">
+                <div className="about-name">
+                    <h1>DANIEL EMANUEL FUNARU</h1>
+                    <h2>FULL STACK DEVELOPER</h2>
+                </div>
+                <div className="about-paragraf">
+                    <p> "Battle not with monsters lest you become one.
+                        And if you gaze into the abyss, the abyss gazes also into you."
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
