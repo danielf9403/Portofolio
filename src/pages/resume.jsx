6da702a7f20fab249fbb3878c0ef751364d12222
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-05-04 19:06:16 -0400

    ... resume

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -4,7 +4,17 @@ import React from "react";
 function resume() {
     return(
         <div className="resume">
-            
+            <div className="resume-mane" >
+                <h1>DANIEL EMANUEL FUNARU</h1>
+                <h2>FULL STACK DEVELOPER</h2>
+            </div>
+            <div className="resume-summary">
+                <h2>PROFESSIONAL SUMMARY</h2>
+                <p>
+                Agile problem solver qualified using RESTful APIs, Python, Node.js, and React to build scalable and efficient products.
+                As a detail-focused developer with expertise in full-stack development, I am deeply committed to expanding my skill set and innovating new applications.
+                </p>
+            </div>
             <div>
                 <h2>SKILLS</h2>
                 <ul>
@@ -20,11 +30,19 @@ function resume() {
             </div>
             <div>
                 <h2>EDUCATION</h2>
-                <ul>
-                    <li>
-                        SCHOOL
-                    </li>
-                </ul>
+                <h3>High school </h3>
+                <p>Technical College of Communications Nicolae Vasilescu- Karpen. Computer technology technician.</p>
+                <p>Bacau, Romania 2008-2013</p>
+                <h3>Certification</h3>
+                <p>North Carolina State Universities Boot camp of Software Development.</p>
+                <p>January 2023</p>
+            </div>
+            <div>
+                <h2>PROFESSIONAL EXPERIENCE</h2>
+                <h3>Chops Inc, Philadelphia PA</h3>
+                <h3>Waiter and bartender</h3>
+                <h3>February 2016–Present</h3>
+                <p>Food Service Supervisors, sets tables, takes orders and serves meals and drinks (bar) to members and guests according to well established rules of etiquette and performs various incidental duties to provide satisfactory service.</p>
             </div>
         </div>
     )
